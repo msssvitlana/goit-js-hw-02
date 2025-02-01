16e48ae7e66540f491d794fe9be8c557f29b32fc
Svitlana <missis.chuvaeva@ukr.net>
2025-02-02 01:29:38 +0200

    new-changes

--- js/task-2.js
+++ js/task-2.js
@@ -15,37 +15,3 @@ console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum
 console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
 console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
 console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
-
-// function getSubstring(string, length) {
-//   return string.slice(0, length);
-// }
-// console.log(getSubstring('Hello world', 3));
-
-// const fullName = 'Jacob Mercer';
-// console.log(fullName.slice(3)); // 'Jacob Mercer'
-
-// function normalizeInput(input, to) {
-//   if (to === 'upper') {
-//     return input.toUpperCase();
-//   } else {
-//     return input.toLowerCase();
-//   }
-// }
-// console.log(normalizeInput("This ISN'T SpaM", 'lower'));
-// console.log(normalizeInput("This ISN'T SpaM", 'lower'));
-// console.log(normalizeInput('Stay Awhile and Listen', 'upper'));
-// console.log(normalizeInput("This ISN'T SpaM", 'upper'));
-// console.log(normalizeInput('Big SALE', 'lower'));
-// console.log(normalizeInput('Big SALE', 'upper'));
-// console.log(normalizeInput('Stay Awhile and Listen', 'upper'));
-
-// function checkForName(fullName, firstName) {
-//   fullName.toLowerCase();
-//   firstName.toLowerCase();
-//   if (fullName.includes('firstName')) {
-//     return (checkForName = true);
-//   } else {
-//     return (checkForName = false);
-//   }
-// }
-// console.log(checkForName('Jason Neis', 'Jacob'));
--- js/task-4.js
+++ js/task-4.js
@@ -1,19 +1,19 @@
 function getShippingCost(country) {
   switch (country) {
     case 'China':
-      let price = '100';
+      price = '100';
       return `Shipping to ${country} will cost ${price} credits`;
       break;
     case 'Chile':
-      let price = '250';
+      price = '250';
       return `Shipping to ${country} will cost ${price} credits`;
       break;
     case 'Australia':
-      let price = '170';
+      price = '170';
       return `Shipping to ${country} will cost ${price} credits`;
       break;
     case 'Jamaica':
-      let price = '120';
+      price = '120';
       return `Shipping to ${country} will cost ${price} credits`;
       break;
 
@@ -21,6 +21,7 @@ function getShippingCost(country) {
       return 'Sorry, there is no delivery to your country';
   }
 }
+
 console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost('Germany')); // "Sorry, there is no delivery to your country"
 console.log(getShippingCost('China')); // "Shipping to China will cost 100 credits"
